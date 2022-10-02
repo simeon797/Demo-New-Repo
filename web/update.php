80e7b6dd4f22cad6c7d8d558e05c4ae9c09d5e75
Simeon Mechkov <crab_1993@abv.bg>
2022-10-02 12:44:51 +0300

    test commit

--- web/update.php
+++ web/update.php
@@ -12,7 +12,7 @@ use Drupal\Core\Update\UpdateKernel;
 use Symfony\Component\HttpFoundation\Request;
 
 $autoloader = require_once 'autoload.php';
-
+$testCommit = 1234;
 // Disable garbage collection during test runs. Under certain circumstances the
 // update path will create so many objects that garbage collection causes
 // segmentation faults.
